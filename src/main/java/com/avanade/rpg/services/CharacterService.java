9df8bb05a70a48e7f120ede8630da0d29e328940
Volnei Neves <volneinevesfilho@hotmail.com>
2023-09-11 20:51:01 -0300

    refactor(services): Refactor exceptions by create character method

--- src/main/java/com/avanade/rpg/services/CharacterService.java
+++ src/main/java/com/avanade/rpg/services/CharacterService.java
@@ -1,12 +1,16 @@
 package com.avanade.rpg.services;
 
 import com.avanade.rpg.entities.Character;
+import com.avanade.rpg.exceptions.ConstraintViolationException;
+import com.avanade.rpg.exceptions.ResourceAlreadyExistsException;
+import com.avanade.rpg.exceptions.UnknownViolationException;
 import com.avanade.rpg.mappers.CharacterMapper;
 import com.avanade.rpg.payloads.requests.CharacterRequest;
 import com.avanade.rpg.payloads.responses.CharacterResponse;
 import com.avanade.rpg.repositories.CharacterRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -32,7 +36,7 @@ public class CharacterService {
         LOGGER.info("Validating existence of character with name: {}", name);
         boolean existsByName = repository.existsByName(name);
         if (existsByName) {
-            LOGGER.warn("Character already exist with the name: {}", name); // TODO handle exception
+            throw new ResourceAlreadyExistsException("Character already exist with the name: " + name);
         }
     }
 
@@ -40,8 +44,10 @@ public class CharacterService {
         try {
             repository.save(character);
             LOGGER.info("Successfully saved character with ID: {}", character.getId());
-        } catch (Exception e) {
-            LOGGER.error("Failed to save character: {} due to: {}", character, e.getMessage()); // TODO handle exception
+        } catch (DataIntegrityViolationException e) {
+            throw new ConstraintViolationException(e.getMessage());
+        }  catch (Exception e) {
+            throw new UnknownViolationException(e.getMessage());
         }
     }
 }
