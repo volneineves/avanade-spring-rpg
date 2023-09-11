a20d3d472eb2d366300d575ef62ef030daff3188
Volnei Neves <volneinevesfilho@hotmail.com>
2023-09-11 20:46:15 -0300

    feat(exceptions): Add new custom exceptions

---
+++ src/main/java/com/avanade/rpg/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.avanade.rpg.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/avanade/rpg/exceptions/UnknownViolationException.java
@@ -0,0 +1,8 @@
+package com.avanade.rpg.exceptions;
+
+public class UnknownViolationException extends RuntimeException {
+
+    public UnknownViolationException(String message) {
+        super(message);
+    }
+}
