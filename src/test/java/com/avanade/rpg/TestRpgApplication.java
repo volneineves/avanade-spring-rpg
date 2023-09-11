f956c1e022c6e727bc42427d17fda00d72786103
Volnei Neves <volneinevesfilho@hotmail.com>
2023-09-11 18:21:50 -0300

    First commit

---
+++ src/test/java/com/avanade/rpg/TestRpgApplication.java
@@ -0,0 +1,23 @@
+package com.avanade.rpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestRpgApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(RpgApplication::main).with(TestRpgApplication.class).run(args);
+	}
+
+}
