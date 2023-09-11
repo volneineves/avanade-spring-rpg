1ed8ec0a0878ec0600d474668910273f6a71c7e5
Volnei Neves <volneinevesfilho@hotmail.com>
2023-09-11 20:44:21 -0300

    refactor(annotations): Updating message validations in CharacterRequest

--- src/main/java/com/avanade/rpg/payloads/requests/CharacterRequest.java
+++ src/main/java/com/avanade/rpg/payloads/requests/CharacterRequest.java
@@ -1,18 +1,43 @@
 package com.avanade.rpg.payloads.requests;
 
+import com.avanade.rpg.annotations.ValidateCharacterType;
 import com.avanade.rpg.annotations.ValidateDiceFaces;
 import com.avanade.rpg.enums.CharacterType;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
-public record CharacterRequest(@NotNull @Size(min = 1, max = 100) String name,
-                               @NotNull @Positive Short health,
-                               @NotNull Short strength,
-                               @NotNull Short defense,
-                               @NotNull Short agility,
-                               @NotNull @Positive Short numDice,
-                               @NotNull @ValidateDiceFaces Short faces,
-                               @NotNull CharacterType type
+import static com.avanade.rpg.constants.ErrorMessages.*;
+
+public record CharacterRequest(
+        @NotNull(message = NAME_CANNOT_BE_EMPTY_NULL)
+        @Size(min = 1, max = 100, message = NAME_CANNOT_BE_EMPTY_NULL)
+        String name,
+
+        @NotNull(message = HEALTH_MUST_BE_POSITIVE)
+        @Positive(message = HEALTH_MUST_BE_POSITIVE)
+        Short health,
+
+        @NotNull(message = STRENGTH_CANNOT_BE_NULL)
+        Short strength,
+
+        @NotNull(message = DEFENSE_CANNOT_BE_NULL)
+        Short defense,
+
+        @NotNull(message = AGILITY_CANNOT_BE_NULL)
+        Short agility,
+
+        @NotNull(message = NUM_DICE_MUST_BE_POSITIVE)
+        @Positive(message = NUM_DICE_MUST_BE_POSITIVE)
+        Short numDice,
+
+        @NotNull(message = FACES_MUST_BE_VALID)
+        @ValidateDiceFaces(message = FACES_MUST_BE_VALID)
+        Short faces,
+
+        @ValidateCharacterType(message = TYPE_MUST_BE_VALID)
+        @NotNull(message = TYPE_CANNOT_BE_NULL)
+        String type
 ) {
 }
