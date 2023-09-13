e7f15ed504a2662d57a69f9a9e6c98599551f66e
Volnei Neves <volneinevesfilho@hotmail.com>
2023-09-13 20:59:08 -0300

    feat(constants): Add new Error Messages

--- src/main/java/com/avanade/rpg/constants/ErrorMessages.java
+++ src/main/java/com/avanade/rpg/constants/ErrorMessages.java
@@ -16,10 +16,13 @@ public class ErrorMessages {
     public static final String TYPE_MUST_BE_VALID = "Field type must ve valid: MONSTER | HERO";
     public static final String CHARACTER_NOT_FOUND = "Character not found: ";
     public static final String CHARACTER_ALREADY_EXISTS = "Character already exist: ";
-
     public static final String HERO_ID_CANNOT_BE_NULL = "Field cannot be null: heroId";
     public static final String MONSTER_ID_CANNOT_BE_NULL = "Field cannot be null: monsterId";
     public static final String BATTLE_NOT_FOUND = "Battle could not be found: ";
+    public static final String TURN_NOT_FOUND = "Turn could not be found: ";
     public static final String CHARACTER_IS_DIFFERENT= "Character is different than expected type: : ";
+    public static final String TURN_ALREADY_FINISHED = "Turn already finished";
+    public static final String INVALID_CHARACTER_ID = "Invalid character ID for the current battle";
+
 
 }
