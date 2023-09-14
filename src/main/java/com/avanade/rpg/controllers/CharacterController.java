3450e98e01b9ee1ad07182d2ae7212e8d562c323
Volnei Neves <volneinevesfilho@hotmail.com>
2023-09-13 22:05:33 -0300

    feat(controllers, root): Implementing base cacheable

--- src/main/java/com/avanade/rpg/controllers/CharacterController.java
+++ src/main/java/com/avanade/rpg/controllers/CharacterController.java
@@ -4,6 +4,8 @@ import com.avanade.rpg.payloads.requests.CharacterRequest;
 import com.avanade.rpg.payloads.responses.CharacterResponse;
 import com.avanade.rpg.services.CharacterService;
 import jakarta.validation.Valid;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,30 +22,35 @@ public class CharacterController {
         this.characterService = characterService;
     }
 
+    @Cacheable(value = "characters", key = "'all'")
     @GetMapping
     public ResponseEntity<List<CharacterResponse>> getAll() {
         List<CharacterResponse> characters = characterService.getAll();
         return ResponseEntity.ok(characters);
     }
 
+    @Cacheable(value = "characters", key = "'all'")
     @GetMapping("/{id}")
     public ResponseEntity<CharacterResponse> getById(@PathVariable UUID id) {
         CharacterResponse character = characterService.getById(id);
         return ResponseEntity.ok(character);
     }
 
+    @CacheEvict(value = "characters", allEntries = true)
     @PostMapping
     public ResponseEntity<CharacterResponse> create(@RequestBody @Valid CharacterRequest request) {
         CharacterResponse createdCharacter = characterService.create(request);
         return ResponseEntity.status(201).body(createdCharacter);
     }
 
+    @CacheEvict(value = "characters", allEntries = true)
     @PutMapping("/{id}")
     public ResponseEntity<CharacterResponse> update(@PathVariable UUID id, @RequestBody CharacterRequest updatedCharacter) {
         CharacterResponse updated = characterService.update(id, updatedCharacter);
         return ResponseEntity.ok(updated);
     }
 
+    @CacheEvict(value = "characters", allEntries = true)
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
         characterService.delete(id);
